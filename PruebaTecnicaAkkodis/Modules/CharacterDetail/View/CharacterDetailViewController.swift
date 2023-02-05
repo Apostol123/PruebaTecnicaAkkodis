f8f07053d256f03ae08408f73c4d18896af869a3
Apostol, Alexandru <alapostol@deloitte.es>
2023-02-05 12:36:59 +0100

    move content sized table view to its own file

--- PruebaTecnicaAkkodis/Modules/CharacterDetail/View/CharacterDetailViewController.swift
+++ PruebaTecnicaAkkodis/Modules/CharacterDetail/View/CharacterDetailViewController.swift
@@ -117,30 +117,3 @@ extension CharacterDetailViewController: UITableViewDataSourcePrefetching {
         }
     }
 }
-
-
-final class ContentSizedTableView: UITableView {
-    override var contentSize:CGSize {
-        didSet {
-            invalidateIntrinsicContentSize()
-        }
-    }
-
-    override var intrinsicContentSize: CGSize {
-        layoutIfNeeded()
-        return CGSize(width: UIView.noIntrinsicMetric, height: contentSize.height)
-    }
-}
-
-final class ContentSizedCollectionView: UICollectionView {
-    override var contentSize:CGSize {
-        didSet {
-            invalidateIntrinsicContentSize()
-        }
-    }
-
-    override var intrinsicContentSize: CGSize {
-        layoutIfNeeded()
-        return CGSize(width: UIView.noIntrinsicMetric, height: contentSize.height)
-    }
-}
---
+++ PruebaTecnicaAkkodis/Utils/Layout/ContentSizedTableView.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+final class ContentSizedTableView: UITableView {
+    override var contentSize:CGSize {
+        didSet {
+            invalidateIntrinsicContentSize()
+        }
+    }
+
+    override var intrinsicContentSize: CGSize {
+        layoutIfNeeded()
+        return CGSize(width: UIView.noIntrinsicMetric, height: contentSize.height)
+    }
+}
+
+final class ContentSizedCollectionView: UICollectionView {
+    override var contentSize:CGSize {
+        didSet {
+            invalidateIntrinsicContentSize()
+        }
+    }
+
+    override var intrinsicContentSize: CGSize {
+        layoutIfNeeded()
+        return CGSize(width: UIView.noIntrinsicMetric, height: contentSize.height)
+    }
+}
